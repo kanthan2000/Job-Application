c86fad793b0c9381d42d257f05e4fec15d8f0bd5
HARIPRASATH P <97425568+hari7111@users.noreply.github.com>
2022-04-25 20:35:45 +0530

    add core

--- src/components/Form/tabs/gender/Gender.js
+++ src/components/Form/tabs/gender/Gender.js
@@ -2,7 +2,7 @@ import React from 'react';
 import MenuItem from '@material-ui/core/MenuItem';
 import TextField from '@material-ui/core/TextField';
 import { Grid } from '@mui/material';
-import { Box } from '@mui/system';
+import  Box  from '@mui/system';
 
 const genders = [
   {

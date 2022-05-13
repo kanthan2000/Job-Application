31b0325cefd1a7f5bfa65404ada576c48c52e0ab
kanthan2000 <kanthan27112001@gmail>
2022-05-13 10:32:53 +0100

    fixed change

--- src/components/Form/tabs/skill/SkillsList.js
+++ src/components/Form/tabs/skill/SkillsList.js
@@ -1,10 +1,5 @@
-import * as React from 'react';
 import { useTheme } from '@mui/material/styles';
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Checkbox from '@mui/material/Checkbox';
-import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
 import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
 import CheckBoxIcon from '@mui/icons-material/CheckBox';
 import { Box } from '@mui/system';
